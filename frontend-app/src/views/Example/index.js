2cbb61febb160877b9e6a2220e1d0ce09056996a
carlos <carenas@bloobirds.com>
2019-11-22 19:57:51 +0100

    status

--- frontend-app/src/views/Example/index.js
+++ frontend-app/src/views/Example/index.js
@@ -3,14 +3,16 @@ import React, {useEffect, useState} from 'react';
 function Index(props) {
   //Example: hola estudiar react mola mucho / Hola Estudiar React Mola Mucho
   const capitelize = input => {
-    return input.split(" ").map(w => w.charAt(0).toUpperCase() + w.substring(1)).reduce((acc, curr) => `${acc} ${curr}`);
+    return input.split(" ")
+      .map(w => w.charAt(0).toUpperCase() + w.substring(1))
+      .reduce((acc, curr) => `${acc} ${curr}`);
   };
 
   //Example: hola estudiar react mola mucho / HOLA estudiar REACT mola MUCHO
   const swapCase = input => {
-    const result = input;
-    //Code here
-    return result;
+    return input.split(" ")
+      .map((w, i) => i % 2 === 0 ? w.toUpperCase() : w)
+      .reduce((acc, curr) => `${acc} ${curr}`);
   };
 
   //Example: hola estudiar react mola mucho / {h: 2, o: 3, l: 2, a: 4, " ": 4, …}
