b68998b3e72c8cd46b1d6b787defaf2b331abfb9
carlos <carenas@bloobirds.com>
2019-11-25 17:13:14 +0100

    status

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ProductList from "./views/Pokemons/PokemonList";
+import PokemonList from "./views/Pokemons/PokemonList";
 import {APP_PRODUCT_ID, APP_PRODUCTS, APP_ROOT} from "./routes/routes";
 import {  BrowserRouter , Route } from 'react-router-dom';
 import Example from "./views/Example";
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Route exact path={APP_PRODUCTS}  component={ProductList} />
+      <Route exact path={APP_PRODUCTS}  component={PokemonList} />
       <Route exact path={APP_PRODUCT_ID}  component={Pokemon} />
       <Route exact path={APP_ROOT}  component={Example} />
     </BrowserRouter>
--- frontend-app/src/views/Pokemons/Pokemon.js
+++ frontend-app/src/views/Pokemons/Pokemon.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import {TextField} from "@material-ui/core";
 import {useParams} from "react-router-dom";
 
 function Pokemon(props) {
@@ -8,7 +7,33 @@ function Pokemon(props) {
   const [pokemon, setPokemon] = useState();
 
   useEffect(() => {
-   //your code here
+    const fetchData = async () => {
+      const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+      const options = {
+        method: 'GET',
+        headers: new Headers({
+          Accept: 'application/json',
+          'Access-Control-Allow-Headers': 'Authorization',
+          'Content-Type': 'application/json',
+        }),
+        mode: 'cors',
+      };
+
+      return fetch(url, options)
+        .then(response => {
+          if (response.status === 200) {
+            return response.json();
+          }
+          return Promise.reject(response.status);
+        })
+        .then(data => {
+          debugger;
+          setPokemon(data);
+        })
+        .catch(error => console.log(error));
+    };
+
+    fetchData();
   }, []);
 
 
