f18c0ada5916566fc6ac302e4cd7c85e88173707
carlosarenasf <carenas@bloobirds.com>
2020-07-10 21:35:23 +0200

    FORM and POST

--- frontend-app/src/pages/home/home.view.js
+++ frontend-app/src/pages/home/home.view.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from './home.module.css';
 import PinCard from "../../components/pinCard/pinCard.view";
 import ListPins from "../../components/listPins/listPins.view";
+import PinForm from "../../components/pinForm/pinForm.view";
 
 const Home = () => {
   return (
     <div className={styles.__home_container}>
-      <ListPins />
+      <PinForm />
     </div>
   );
 };
